9bc44ac8628c41cbbbdacf9a22044dbc18ea3ed3
TheMGRF <thomas.grf@googlemail.com>
2020-06-26 09:41:18 +0100

    Start testing with shaders

--- src/main/java/me/themgrf/avalon/Start.java
+++ src/main/java/me/themgrf/avalon/Start.java
@@ -4,6 +4,7 @@ import me.themgrf.avalon.renderer.DisplayManager;
 import me.themgrf.avalon.renderer.models.Loader;
 import me.themgrf.avalon.renderer.models.RawModel;
 import me.themgrf.avalon.renderer.models.Renderer;
+import me.themgrf.avalon.renderer.shaders.StaticShader;
 import org.lwjgl.opengl.Display;
 
 public class Start {
@@ -16,6 +17,7 @@ public class Start {
 
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
 
         float[] vertices = {
                 -0.5f, 0.5f, 0,
@@ -29,18 +31,18 @@ public class Start {
                 3,1,2
         };
 
-        // 0, 1, 3, 3, 1, 2
-
         RawModel rawModel = loader.loadToVAO(vertices, indices);
 
         while (!Display.isCloseRequested()) {
-
             renderer.prepare();
+            shader.start();
             renderer.render(rawModel);
+            shader.stop();
 
             DisplayManager.updateDisplay();
         }
 
+        shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
     }
