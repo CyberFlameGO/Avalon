a443caa6b9dc25ad0c9148b0320a5ef983e00424
TheMGRF <thomas.grf@googlemail.com>
2020-07-04 23:30:17 +0100

    Add Camera zooming with Shift multiplier

--- src/main/java/me/themgrf/avalon/entities/Camera.java
+++ src/main/java/me/themgrf/avalon/entities/Camera.java
@@ -1,13 +1,15 @@
 package me.themgrf.avalon.entities;
 
+import me.themgrf.avalon.utils.Logger;
 import me.themgrf.avalon.utils.Rotation;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 
-    private Vector3f position = new Vector3f(0, 0, 0);
-    private Rotation rotation = new Rotation(0, 0, 0);
+    private final Vector3f position = new Vector3f(0, 0, 0);
+    private final Rotation rotation = new Rotation(0, 0, 0);
 
     public void move() {
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
@@ -21,15 +23,17 @@ public class Camera {
             position.x -= 0.02f;
         }
 
-        // TODO: Figure out how on earth mouse works
-        /*int dWheel = Mouse.getDWheel();
-        if (dWheel != 0) {
-            // up
-            position.z -= 0.02f;
-        } else {
-            // down
-            position.z += 0.02f;
-        }*/
+        int dWheel = Mouse.getDWheel();
+        int multiplier = Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) ? 4 : 1;
+        float increment = 0.05f;
+        float total = increment * multiplier;
+
+        if (dWheel < 0) { // zoom in
+            position.z += total;
+        } else if (dWheel > 0) { // zoom out
+            position.z -= total;
+        }
+
     }
 
     public Vector3f getPosition() {
