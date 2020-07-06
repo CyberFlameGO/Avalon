15b6d24e1f1df31a29f553afe411a55fe985eaa3
TheMGRF <thomas.grf@googlemail.com>
2020-07-06 21:56:49 +0100

    Add Shift multiplier to movement keys

--- src/main/java/me/themgrf/avalon/entities/Camera.java
+++ src/main/java/me/themgrf/avalon/entities/Camera.java
@@ -11,21 +11,25 @@ public class Camera {
     private final Rotation rotation = new Rotation(0, 0, 0);
 
     public void move() {
+        int multiplier = Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) ? 4 : 1;
+
+        float increment = 0.02f;
+        float total = increment * multiplier;
+
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            position.y += 0.02f;
+            position.y += total;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            position.y -= 0.02f;
+            position.y -= total;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            position.x += 0.02f;
+            position.x += total;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            position.x -= 0.02f;
+            position.x -= total;
         }
 
         int dWheel = Mouse.getDWheel();
-        int multiplier = Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) ? 4 : 1;
-        float increment = 0.05f;
-        float total = increment * multiplier;
+        increment = 0.05f;
+        total = increment * multiplier;
 
         if (dWheel < 0) { // zoom in
             position.z += total;
